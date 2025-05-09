4fff9527d72e332a40a61927aac4321cbd2c3032
Yogibear-pixel-99 <joachim.puercher@gmail.com>
2025-05-09 12:27:30 +0200

    added bottles to canvas

--- js/level.class.js
+++ js/level.class.js
@@ -1,12 +1,14 @@
 class Level {
     coins;
+    bottles;
     enemies;
     skyObjects;
     backgrounds;
     level_end_x;
 
-    constructor(coins, enemies, clouds, background, level_end_x){
+    constructor(coins, bottles, enemies, clouds, background, level_end_x){
         this.coins = coins;
+        this.bottles = bottles;
         this.enemies = enemies;
         this.skyObjects = clouds;
         this.backgrounds = background;
--- js/levels/level1.js
+++ js/levels/level1.js
@@ -16,6 +16,12 @@ const level1 = new Level(
 
   ],
 
+    [
+      new Bottle(),
+      new Bottle(),
+      new Bottle()
+    ],
+
   [
     new Chicken(),
     new Chicken(),
