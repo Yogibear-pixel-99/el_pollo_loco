bc3590911d7461c913b2888adb7a145ce2ce2e91
Yogibear-pixel-99 <joachim.puercher@gmail.com>
2025-05-10 20:38:50 +0200

    added pointsTableRender

--- js/game.js
+++ js/game.js
@@ -13,12 +13,13 @@ const highscores = [
   { name: "Gustav", score: 770 },
   { name: "Hanna", score: 750 },
   { name: "Isa", score: 700 },
-  { name: "Jonas", score: 680 }
+  { name: "Jonas", score: 680 },
 ];
 
 // let highscores = {};
 
-const MAIN_URL = "https://el-pollo-loco-79444-default-rtdb.europe-west1.firebasedatabase.app/";
+const MAIN_URL =
+  "https://el-pollo-loco-79444-default-rtdb.europe-west1.firebasedatabase.app/";
 
 function init() {
   canvas = document.getElementById("gamecanvas");
@@ -26,53 +27,104 @@ function init() {
   getHighscores();
 }
 
-function getHighscores(){
+function getHighscores() {
   // get high von api
   // wenn leer füge template ein.
   renderHighscore();
+  renderGamePointsTable();
   // bei spielende namen und highscore passend ins object einfügen.
   // object in die database - PUT
   // neu im HTML rendern.
-
 }
 
-async function getHighscoreFromApi(){
+async function getHighscoreFromApi() {
   try {
-    let response = await fetch (MAIN_URL + ".json");
-      if (!response.ok) {
-        throw new Error();
-      } else {
-        let data = await response.json();
-        if (data) {
-          highscores = Object.values(data);
-        }
+    let response = await fetch(MAIN_URL + ".json");
+    if (!response.ok) {
+      throw new Error();
+    } else {
+      let data = await response.json();
+      if (data) {
+        highscores = Object.values(data);
       }
+    }
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
-function renderHighscore(){
-  let ref = document.getElementById('highscore');
-  let data = '';
-      for (let index = 0; index < highscores.length; index++) {
-        const element = highscores[index];
-        data += highscoreTemp(element);
-        
-      }
-    ref.innerHTML = data;
+function renderHighscore() {
+  let ref = document.getElementById("highscore");
+  let data = "";
+  for (let index = 0; index < highscores.length; index++) {
+    const element = highscores[index];
+    data += highscoreTemp(element);
+  }
+  ref.innerHTML = data;
 }
 
 function highscoreTemp(element) {
   return `<div class="score-wrapper">
             <div class="player-name">${element.name}</div>
             <div class="score">${element.score}</div>          
-          </div>`
+          </div>`;
 }
 
-// 
+function getPointsTemp(element) {
+  return `<div class="item-point-wrapper">
+            <span class="item-name">${element[1].name}</span>
+            <span class="item-points" id="${element[0]}">${element[1].points}</span>
+          </div>
+  `;
+}
 
 
+function renderGamePointsTable(){
+  let ref = document.getElementById('game-points-table');
+  let data = '';
+      Object.entries(pointConfig).forEach((element) => {
+        console.log(element);
+        data += getPointsTemp(element);
+      })
+    ref.innerHTML = data;
+}
+
+const pointConfig = {
+  miniChickenJump: {
+    name: "mini chicken jump",
+    points: 30,
+  },
+  chickenBottleHit: {
+    name: "chicken bottle hit",
+    points: 20,
+  },
+  chickenJump: {
+    name: "chicken jump",
+    points: 10,
+  },
+  collectCoin: {
+    name: "collect coin",
+    points: 60,
+  },
+  collectBottle: {
+    name: "collect bottle",
+    points: 20,
+  },
+  bottleMissed: {
+    name: "bottle missed",
+    points: -30,
+  },
+  endbossHit: {
+    name: "endboss hit",
+    points: 50,
+  },
+  endbossKilled: {
+    name: "endboss killed",
+    points: 200,
+  },
+};
+
+console.log(Object.entries(pointConfig))
 
 window.addEventListener("keydown", (event) => {
   switch (event.key) {
@@ -145,4 +197,3 @@ window.addEventListener("keyup", (event) => {
       break;
   }
 });
-
